7e1df959010cade55f32ded1b1b0d02adc015cfe
devhong97 <devhong97@gmail.com>
2024-05-14 16:38:48 +0900

    20241514

--- src/component/Board/EducationDetail.jsx
+++ src/component/Board/EducationDetail.jsx
@@ -1,35 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const EducationDetail = () => {
   const { category } = useParams();
+  const [data, setData] = useState([]);
 
-  // 각 category에 따라 다른 JSX 출력
+  //교육 상세데이터
+  const categoryDataSet = (category) => {
+    axios
+      .get(`http://localhost:3001/api/education/${category}`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    categoryDataSet(category);
+  }, [category]);
+
+  // 각 카테고리별 테이블 내용
+  const renderTable = () => {
+    return (
+      <div>
+        <div>
+          <div >직원정보 등록</div>
+        </div>
+        <div>
+          <input type="text" placeholder="검색..." />
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>이름</th>
+              <th>직위</th>
+              <th>업무</th>
+              <th>전화번호</th>
+              <th>이메일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((employee, index) => (
+              <tr key={index}>
+                <td>{employee.name}</td>
+                <td>{employee.spot}</td>
+                <td>{employee.work}</td>
+                <td>{employee.tel}</td>
+                <td>{employee.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
   let content = null;
+
   switch (category) {
     case "management":
-      content = <p>직원 정보1의 상세 내용</p>;
+      content = renderTable();
       break;
     case "enterprise":
-      content = <p>직원 정보2의 상세 내용</p>;
+      content = renderTable();
       break;
     case "resources":
-      content = <p>직원 정보3의 상세 내용</p>;
+      content = renderTable();
       break;
     case "founded":
-      content = <p>직원 정보4의 상세 내용</p>;
+      content = renderTable();
       break;
     default:
       content = <p>잘못된 카테고리입니다.</p>;
   }
 
-  return (
-    <div>
-      <h2>Education Detail</h2>
-      <p>category: {category}</p>
-      {content} {/* 조건부 렌더링 */}
-    </div>
-  );
+  return <div>{content}</div>;
 };
 
 export default EducationDetail;
